ae3cca5f76e3bfcc32eadf37e9fbf343ed9bd36d
theRobMay <robbie.maysent@gmail.com>
2023-09-14 08:50:07 -0500

    Adding framework for flask app - this still needs heavy edits

---
+++ app.py
@@ -0,0 +1,56 @@
+from flask import Flask, render_template, redirect, request, jsonify
+from modelHelper import ModelHelper
+
+# Create an instance of Flask
+app = Flask(__name__)
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+
+modelHelper = ModelHelper()
+
+# Route to render index.html template using data from Mongo
+@app.route("/")
+def home():
+    # Return template and data
+    return render_template("index.html")
+
+@app.route("/about_us")
+def about_us():
+    # Return template and data
+    return render_template("about_us.html")
+
+@app.route("/tableau")
+def tableau():
+    # Return template and data
+    return render_template("tableau2.html")
+
+@app.route("/makePredictions", methods=["POST"])
+def make_predictions():
+    content = request.json["data"]
+    print(content)
+
+    # parse
+    pokemon1 = int(content["pokemon1"])
+    pokemon2 = int(content["pokemon2"])
+
+
+    preds = modelHelper.makePredictions(pokemon1,pokemon2)
+    return(jsonify({"ok": True, "prediction": str(preds)}))
+
+
+#############################################################
+
+@app.after_request
+def add_header(r):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    r.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate, public, max-age=0"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    return r
+
+#main
+if __name__ == "__main__":
+    app.run(debug=True)
